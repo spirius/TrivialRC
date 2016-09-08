ac8cf76fc1640b8fe554d0189e1aa9decf7596ac
Oleksii Tsvietnov <me@vorakl.name>
2016-09-09 00:57:35 +0200

    Added collecting exitcodes, stdout and stderr

--- ex_1.sh
+++ ex_1.sh
@@ -3,13 +3,38 @@
 source asyncproc
 
 export ASYNCPROC_VERBOSE=true
-export ASYNCPROC_ERRORS=true
 
 trap asyncproc_exit EXIT
 
 asyncproc_run()
 {
-    sleep 2
+    echo 1234 110 > ${ASYNCPROC_HANDLER_EC_INPUT}
+    echo 1235 0 > ${ASYNCPROC_HANDLER_EC_INPUT}
+    echo 1236 112 > ${ASYNCPROC_HANDLER_EC_INPUT}
+    echo 1234 Pinging... > ${ASYNCPROC_HANDLER_STDOUT_INPUT}
+    echo 1235 Listens on the port 200 > ${ASYNCPROC_HANDLER_STDOUT_INPUT}
+    echo 1236 Copying files... > ${ASYNCPROC_HANDLER_STDOUT_INPUT}
+    echo 1234 The host is unreachble > ${ASYNCPROC_HANDLER_STDERR_INPUT}
+
+    asyncproc_get_exitcodes "dump"
+    asyncproc_get_stdouts "dump"
+    asyncproc_get_stderrs "dump"
+    asyncproc_say "Workers: ${asyncproc_workers[*]}"
+    asyncproc_say "Exit codes: ${asyncproc_exitcodes[*]}"
+    asyncproc_say "Files with Stdout: ${asyncproc_stdouts[*]}"
+    asyncproc_say "Files with Stderr: ${asyncproc_stderrs[*]}"
+    asyncproc_workers=()
+    asyncproc_exitcodes=()
+    asyncproc_stdouts=()
+    asyncproc_stderrs=()
+
+    echo 1237 114 > ${ASYNCPROC_HANDLER_EC_INPUT}
+    echo 1238 130 > ${ASYNCPROC_HANDLER_EC_INPUT}
+    echo 1239 0 > ${ASYNCPROC_HANDLER_EC_INPUT}
+    echo 1237 Done > ${ASYNCPROC_HANDLER_STDOUT_INPUT}
+    echo 1238 Just loading... > ${ASYNCPROC_HANDLER_STDOUT_INPUT}
+    echo 1239 Info about something > ${ASYNCPROC_HANDLER_STDOUT_INPUT}
+    echo 1238 The file does not exist > ${ASYNCPROC_HANDLER_STDERR_INPUT}
 }
 
 asyncproc_start
