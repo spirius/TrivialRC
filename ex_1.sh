0dce7f6f8d41abe45eef08ac2d25560067e05656
Oleksii Tsvietnov <me@vorakl.name>
2016-09-08 02:13:15 +0200

    term handlers

--- ex_1.sh
+++ ex_1.sh
@@ -5,17 +5,13 @@ source asyncproc
 export ASYNCPROC_VERBOSE=true
 export ASYNCPROC_ERRORS=true
 
+trap asyncproc_exit EXIT
+
 asyncproc_run()
 {
-    asyncproc_say "Hello from run..."
     sleep 2
 }
 
-asyncproc_handler_ec_conf_local() {
-    ASYNCPROC_HANDLER_EC_INPUT="/asd/dsa"
-}
-
 asyncproc_start
-trap asyncproc_exit EXIT
-
 asyncproc_run
+
